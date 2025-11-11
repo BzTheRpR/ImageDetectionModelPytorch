2ffaaee248fdf454542dc6183f73187496029803
BzTheRpR <bjbd74@gmail.com>
2025-11-11 12:48:19 +0600

    added libs

---
+++ codes/ImageProcessingModel.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transformd
+from torchvision.datasets import ImageFolder
+import time
+import matplotlib.pyplot as plt
+import pandas as pd
+
+print("HI")
